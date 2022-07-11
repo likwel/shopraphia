38b549a9ba64d8befdc7a1295929749f8e650724
lykely <57036957+lykely@users.noreply.github.com>
2022-07-11 17:15:25 +0300

    Update lien footer facebook

--- Vues/footer.php
+++ Vues/footer.php
@@ -28,7 +28,7 @@
 		 </div>
 		<div id="socialMedia" class="span3 pull-right">
 			<h5>RESEAU SOCIAL</h5>
-			<a href="#"><img width="60" height="60" src="themes/images/facebook.png" title="facebook" alt="facebook"/></a>
+			<a href="https://www.facebook.com/shopraphia.mg/"><img width="60" height="60" src="themes/images/facebook.png" title="facebook" alt="facebook"/></a>
 			<a href="#"><img width="60" height="60" src="themes/images/twitter.png" title="twitter" alt="twitter"/></a>
 			<a href="#"><img width="60" height="60" src="themes/images/youtube.png" title="youtube" alt="youtube"/></a>
 		 </div> 
